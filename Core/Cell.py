f0c8d49dd5e42c24ca3dbb9189a8e07f0df148a9
akimjlord-p <81712770+akimjlord-p@users.noreply.github.com>
2025-02-11 19:52:58 +0400

    Cell class added akimjlord

--- Core/Cell.py
+++ Core/Cell.py
@@ -1,20 +1,20 @@
-import os
-import sys
 import pygame
 
 
 class Cell(pygame.sprite.Sprite):
-    image = pygame.image.load('../media/Cell.png')
+    sprite_sheet = alive_cell = pygame.image.load('../media/Cell.png')
+    image = dead_cell = pygame.image.load('../media/dead_cell.png')
 
-    def __init__(self, group, x, y):
+    def __init__(self, group, x, y, x_indent, y_indent):
+        self.is_alive = False
         super().__init__(group)
         self.frames = []
         self.cur_frame = 0
-        self.cut_sheet(Cell.image)
+        self.cut_sheet(Cell.alive_cell)
         self.rect = pygame.rect.Rect(0, 0, 100, 100)
 
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = x * 100 + x_indent
+        self.rect.y = y * 100 + y_indent
 
     def cut_sheet(self, sheet):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // 3,
@@ -25,27 +25,41 @@ class Cell(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
+    def revive(self):
+        self.is_alive = True
+        self.cur_frame = 0
+
+    def kill(self):
+        self.is_alive = False
+
+    def get_status(self):
+        return self.is_alive
+
     def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
+        if self.is_alive:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+        else:
+            self.image = Cell.dead_cell
 
+# Для теста
+# size = width, height = 1920, 1000
+# screen = pygame.display.set_mode(size)
+# all_sprites = pygame.sprite.Group()
+# pygame.display.set_caption('Инициализация игры')
 
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
-all_sprites = pygame.sprite.Group()
-pygame.display.set_caption('Инициализация игры')
+# Cell(all_sprites, 0, 0, 300, 300).kill()
+# Cell(all_sprites, 1, 1, 300, 300)
+# clock = pygame.time.Clock()
+# running = True
 
-Cell(all_sprites, 100, 100)
-clock = pygame.time.Clock()
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        clock.tick(10)
-        screen.fill((255, 255, 255))
-        all_sprites.update()
-        all_sprites.draw(screen)
-        pygame.display.flip()
-pygame.quit()
-
+# while running:
+#    for event in pygame.event.get():
+#        if event.type == pygame.QUIT:
+#            running = False
+#    clock.tick(10)
+#    screen.fill((255, 255, 255))
+#    all_sprites.update()
+#   all_sprites.draw(screen)
+#    pygame.display.flip()
+# pygame.quit()
